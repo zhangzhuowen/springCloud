a1c69bc3d65b522a58fae8b1307b018cced68921
zhangzhuowen <zhaozhuowen@navinfo.com>
2021-03-16 14:02:43 +0800

    完结，撒花

---
+++ springcloud-config-client-3355/src/main/java/com/navinfo/springcloud/ConfigClient_3355.java
@@ -0,0 +1,17 @@
+package com.navinfo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhangZhuoWen
+ * @ClassName ConfigClient_3355
+ * @date 2021/2/4 11:22
+ * @Description TODO
+ */
+@SpringBootApplication
+public class ConfigClient_3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355.class, args);
+    }
+}
---
+++ springcloud-config-client-3355/src/main/java/com/navinfo/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,30 @@
+package com.navinfo.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhangZhuoWen
+ * @ClassName ConfigClientController
+ * @date 2021/3/2 17:46
+ * @Description TODO
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${spring.application.name}")
+    private String applicationName; //获取微服务名称
+
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String eurekaServer; //获取Eureka服务
+
+    @Value("${server.port}")
+    private String port; //获取服务端的端口号
+
+    @RequestMapping("/config")
+    public String getConfig(){
+        return "applicationName:"+applicationName +
+                "eurekaServer:"+eurekaServer +
+                "port:"+port;
+    }
+}
